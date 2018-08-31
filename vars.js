32c9f486b0fcd16fc344afc2a43d0fa6a7af927e
0cavila <42749704+0cavila@users.noreply.github.com>
2018-08-31 13:53:44 -0500

    done

--- vars.js
+++ vars.js
@@ -1,8 +1,8 @@
 let a = 10;
 let b = -25;
 //create two more variables c and d and give them whatever number you want.
-let c=100;
-
+let c = 100;
+let d = -100;
 const total = a+b+c+d;
 console.log(total);
 
@@ -12,13 +12,13 @@ console.log(total);
 //new section on string variables
 let convo = "hello ";
 //set name to your name, first and last.
-let name;
+let name = "Christian Avila"; 
 //add string convo and name together.
-const greeting =
+const greeting = convo + name
 console.log(greeting);
 
 //make your own message similar to what is above.
-let convo2;
-let
-const
-console.log();
+let convo2 = "greetings ";
+let convo3 = "Billy ";
+const games = convo2 + convo3;
+console.log(games);
